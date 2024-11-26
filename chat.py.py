36b931c5e251edccc0b6e7e03cad83b347383095
woongabs16 <76767084+woongabs16@users.noreply.github.com>
2024-11-26 13:50:54 +0900

    Update chat.py.py

--- chat.py.py
+++ chat.py.py
@@ -20,7 +20,7 @@ os.environ["ANTHROPIC_API_KEY"] = "'sk-proj-lqzkA7aign9K5bu3oXa6uf_o-rpr_9_lc9zP
 
 import streamlit as st
 from PyPDF2 import PdfReader
-'''from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_community.embeddings.spacy_embeddings import SpacyEmbeddings
 from langchain_community.vectorstores import FAISS
@@ -28,7 +28,7 @@ from langchain.tools.retriever import create_retriever_tool
 from dotenv import load_dotenv
 from langchain_anthropic import ChatAnthropic
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
-from langchain.agents import AgentExecutor, create_tool_calling_agent'''
+from langchain.agents import AgentExecutor, create_tool_calling_agent
 
 import os
 os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
